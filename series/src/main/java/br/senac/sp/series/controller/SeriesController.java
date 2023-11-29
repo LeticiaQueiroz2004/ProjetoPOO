5fbf8850040ba04fb26c021c36cb07ad5aa02f98
Alexsandro Alves <Alex@adm-info.com>
2023-11-29 02:27:51 -0300

    Adicionando o btn de apagar, config para auth do git e ajuste do css

--- series/src/main/java/br/senac/sp/series/controller/SeriesController.java
+++ series/src/main/java/br/senac/sp/series/controller/SeriesController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -37,6 +39,12 @@ public class SeriesController {
 
         repository.save(serie);
 
+        return "redirect:/series";
+    }
+
+     @DeleteMapping("{id}")
+    public String delete(@PathVariable Long id) {
+        repository.deleteById(id);
         return "redirect:/series";
     }
 
